54cd40e533f4bd35f00daffaa0e09a77f67b0dff
Kerouz <luc@Office-iMac.local>
2013-09-01 17:34:59 +1000

    Initial Commit

---
+++ TwitterAssignment/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TwitterAssignment
+//
+//  Created by Luc Le Quiniat on 1/09/13.
+//  Copyright (c) 2013 Luc Le Quiniat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TwitterAssignment/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TwitterAssignment
+//
+//  Created by Luc Le Quiniat on 1/09/13.
+//  Copyright (c) 2013 Luc Le Quiniat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
